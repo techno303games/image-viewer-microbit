3fde3a07e1451230f2dafee3d724d924b315dfbf
techno303games <129646624+techno303games@users.noreply.github.com>
2023-04-03 18:39:37 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+basic.showString("m")
+basic.pause(50)
+basic.showString("i")
+basic.pause(50)
+basic.showString("c")
+basic.pause(50)
+basic.showString("r")
+basic.pause(50)
+basic.showString("o")
+basic.pause(50)
+basic.showString(":")
+basic.pause(50)
+basic.showString("b")
+basic.pause(50)
+basic.showString("i")
+basic.pause(50)
+basic.showString("t")
+basic.pause(50)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(50)
+basic.showLeds(`
+    . . . . .
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(50)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(50)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    # # # # #
+    # # # # #
+    `)
+basic.pause(50)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    # # # # #
+    `)
+basic.pause(50)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(50)
+basic.clearScreen()
