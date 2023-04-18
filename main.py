b29617de53339d8baf0d59441389297578385301
technoferret303 <129646624+techno303games@users.noreply.github.com>
2023-04-18 14:59:18 +1000

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,86 @@
+basic.show_string("smile")
+basic.show_leds("""
+    # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        # # # # #
+""")
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("happy")
+basic.show_icon(IconNames.HAPPY)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("sad")
+basic.show_icon(IconNames.SAD)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("ghost")
+basic.show_icon(IconNames.GHOST)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("board")
+basic.show_icon(IconNames.CHESSBOARD)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("fork")
+basic.show_icon(IconNames.PITCHFORK)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("meh")
+basic.show_icon(IconNames.MEH)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("cool")
+basic.show_icon(IconNames.FABULOUS)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("meh")
+basic.show_icon(IconNames.MEH)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("house")
+basic.show_icon(IconNames.HOUSE)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("silly")
+basic.show_icon(IconNames.SILLY)
+basic.pause(100)
+basic.clear_screen()
+basic.show_string("tick")
+basic.show_icon(IconNames.YES)
+basic.pause(100)
+basic.clear_screen()
+
+def on_forever():
+    pass
+basic.forever(on_forever)
+
+def on_forever2():
+    pass
+basic.forever(on_forever2)
+
+def on_forever3():
+    pass
+basic.forever(on_forever3)
+
+def on_forever4():
+    pass
+basic.forever(on_forever4)
+
+def on_forever5():
+    pass
+basic.forever(on_forever5)
+
+def on_forever6():
+    pass
+basic.forever(on_forever6)
+
+def on_forever7():
+    pass
+basic.forever(on_forever7)
+
+def on_forever8():
+    pass
+basic.forever(on_forever8)
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 basic.showString("smile")
 basic.showLeds(`
     # # . # #
-    # # . # #
-    . . . . .
-    # . . . #
-    # # # # #
-    `)
+        # # . # #
+        . . . . .
+        # . . . #
+        # # # # #
+`)
 basic.pause(100)
 basic.clearScreen()
 basic.showString("happy")
@@ -20,6 +20,59 @@ basic.showString("ghost")
 basic.showIcon(IconNames.Ghost)
 basic.pause(100)
 basic.clearScreen()
-basic.forever(function () {
-	
+basic.showString("board")
+basic.showIcon(IconNames.Chessboard)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("fork")
+basic.showIcon(IconNames.Pitchfork)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("meh")
+basic.showIcon(IconNames.Meh)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("cool")
+basic.showIcon(IconNames.Fabulous)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("meh")
+basic.showIcon(IconNames.Meh)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("house")
+basic.showIcon(IconNames.House)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("silly")
+basic.showIcon(IconNames.Silly)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("tick")
+basic.showIcon(IconNames.Yes)
+basic.pause(100)
+basic.clearScreen()
+basic.forever(function on_forever() {
+    
+})
+basic.forever(function on_forever2() {
+    
+})
+basic.forever(function on_forever3() {
+    
+})
+basic.forever(function on_forever4() {
+    
+})
+basic.forever(function on_forever5() {
+    
+})
+basic.forever(function on_forever6() {
+    
+})
+basic.forever(function on_forever7() {
+    
+})
+basic.forever(function on_forever8() {
+    
 })
