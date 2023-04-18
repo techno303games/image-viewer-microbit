157e149349b4dcd6d176c84211c64129408e0080
technoferret303 <129646624+techno303games@users.noreply.github.com>
2023-04-18 14:31:52 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
-basic.showString("Welcome to the micro:bit image viewer!")
-basic.showString("")
-basic.showString("The first image is...")
 basic.showString("SMILE | Basics - techno303games")
+basic.showLeds(`
+    # # . # #
+    # # . # #
+    . . . . .
+    # . . . #
+    # # # # #
+    `)
+basic.pause(100)
+basic.showString("happy | makecode.microbit.org")
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.showString("sad | makecode.microbit.org")
+basic.showIcon(IconNames.Sad)
+basic.pause(100)
+basic.showString("ghost | makecode.microbit.org")
+basic.showIcon(IconNames.Ghost)
+basic.pause(100)
+basic.forever(function () {
+	
+})
