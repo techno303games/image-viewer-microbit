b2f03ab4895f5919e23f3393fe01994f92071450
technoferret303 <129646624+techno303games@users.noreply.github.com>
2023-04-18 14:38:11 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+basic.showString("smile")
+basic.showLeds(`
+    # # . # #
+    # # . # #
+    . . . . .
+    # . . . #
+    # # # # #
+    `)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("happy")
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("sad")
+basic.showIcon(IconNames.Sad)
+basic.pause(100)
+basic.clearScreen()
+basic.showString("ghost")
+basic.showIcon(IconNames.Ghost)
+basic.pause(100)
+basic.clearScreen()
 basic.forever(function () {
 	
 })
