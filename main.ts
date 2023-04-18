afcc4da86ab0294cf596e2a1738cdb3e8d1979db
technoferret303 <129646624+techno303games@users.noreply.github.com>
2023-04-18 14:36:17 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,3 @@
-basic.showString("SMILE | Basics - techno303games")
-basic.showLeds(`
-    # # . # #
-    # # . # #
-    . . . . .
-    # . . . #
-    # # # # #
-    `)
-basic.pause(100)
-basic.showString("happy | makecode.microbit.org")
-basic.showIcon(IconNames.Happy)
-basic.pause(100)
-basic.showString("sad | makecode.microbit.org")
-basic.showIcon(IconNames.Sad)
-basic.pause(100)
-basic.showString("ghost | makecode.microbit.org")
-basic.showIcon(IconNames.Ghost)
-basic.pause(100)
 basic.forever(function () {
 	
 })
